494383183e430d924b9a02d7c1bb59a4f99e5882
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-11-20 10:37:32 +0200

    Configuring Django Media Files

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -118,6 +118,11 @@ STATICFILES_DIRS = [
     "greatkart/static",
 ]
 
+
+MEDIA_URL = 'MEDIA/'
+MEDIA_ROOT = BASE_DIR / 'MEDIA/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
