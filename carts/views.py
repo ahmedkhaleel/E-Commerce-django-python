9a6be30fe96788a3c7749146312288ae3e908843
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-11-24 10:44:14 +0200

    Check for Empty Cart

--- carts/views.py
+++ carts/views.py
@@ -58,6 +58,7 @@ def remove_cart_item(request, product_id):
     cart_item.delete()
     return redirect('cart')
 
+
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
