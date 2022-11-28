db1d66bce011c4f142d0236b2eb3e67f6e15792d
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-11-28 18:30:11 +0200

    Product Variation Model

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import CASCADE
 from django.urls import reverse
 
 from category.models import Category
@@ -22,3 +23,20 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.product
